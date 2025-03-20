72f63e7ce745a6ac8e3c29828aa9a66d16d728d2
jpribeiro-ti <114706918+jpribeiro-ti@users.noreply.github.com>
2025-03-20 20:49:51 -0300

    Delete 001-input.js

--- 001-input.js
+++
@@ -1,8 +0,0 @@
-import entradaDados from 'readline-sync';
-const nome = entradaDados.question('Digite seu nome: ');
-
-
-let idade = entradaDados .question('Digite sua idade: ');
-
-console.log('Olá, '+nome);
-console.log("Você tem "+idade+" anos de idade");
