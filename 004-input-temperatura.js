73c23011c48b98421dc0ffa48e2edb209bb35438
jpribeiro-ti <114706918+jpribeiro-ti@users.noreply.github.com>
2025-03-21 06:08:42 -0300

    Delete 004-input-temperatura.js

--- 004-input-temperatura.js
+++
@@ -1,9 +0,0 @@
-import entradaDados from 'readline-sync';
-
-console.log("Conversor de Celsius para Kelvin: \n");
-
-let C = entradaDados.question("Informe o valor em Celsius: ");
-
-let K = Number(C) + 273.15;
-
-console.log(C + "ºC equivale a :" + K + "K");
